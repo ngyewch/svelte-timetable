2a0d11c61ebc19c11914edd85faea47a3eef2016
Nick Ng <ngyewch@gmail.com>
2022-01-25 14:32:09 +0800

    feat: added Storybook integration

---
+++ src/stories/Timetable.stories.svelte
@@ -0,0 +1,68 @@
+<script lang="ts">
+  import { Meta, Template, Story } from "@storybook/addon-svelte-csf";
+  import Timetable from "../Timetable.svelte";
+  import {Entity, Entry, Group} from "../timetable";
+
+  const groups = [];
+
+  groups.push(
+          new Group('group1')
+                  .addEntity(new Entity('entity1_1')
+                          .addEntry(new Entry('05:00', 'PT1H', 'test1'))
+                  )
+                  .addEntity(new Entity('entity1_2')
+                          .addEntry(new Entry('07:30', 'PT3H20M', 'test2'))
+                  )
+                  .addEntity(new Entity('entity1_3'))
+  );
+  groups.push(
+          new Group('group2')
+                  .addEntity(new Entity('entity2_1'))
+                  .addEntity(new Entity('entity2_2'))
+                  .addEntity(new Entity('entity2_3'))
+  );
+  groups.push(
+          new Group('group3')
+                  .addEntity(new Entity('entity3_1'))
+                  .addEntity(new Entity('entity3_2'))
+                  .addEntity(new Entity('entity3_3'))
+  );
+</script>
+
+<!-- More on default export: https://storybook.js.org/docs/react/writing-stories/introduction#default-export -->
+<!-- More on argTypes: https://storybook.js.org/docs/svelte/api/argtypes -->
+<Meta
+  title="Components/Timetable"
+  component={Timetable}
+  argTypes={{
+    startHour: {
+      control: {
+        type: 'number',
+        min: 0,
+        max: 23,
+        step: 1,
+      }
+    },
+    endHour: {
+      control: {
+        type: 'number',
+        min: 0,
+        max: 23,
+        step: 1,
+      }
+    },
+  }}
+/>
+
+<!-- More on component templates: https://storybook.js.org/docs/svelte/writing-stories/introduction#using-args -->
+<Template let:args>
+  <Timetable {...args} on:click={args.onClick} />
+</Template>
+
+<!-- More on args: https://storybook.js.org/docs/svelte/writing-stories/args -->
+<Story
+  name="General"
+  args={{
+    groups: groups,
+  }}
+/>
