2e1a762bf06b64081f49f495a31e2d5bf3b011b0
Nick Ng <ngyewch@gmail.com>
2022-08-31 16:01:47 +0800

    feat: updated exports

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,7 @@
 import Timetable from './Timetable.svelte';
-import { Entity, Entry, Group } from './timetable';
-export { Entity, Entry, Group, Timetable };
+import {Entity, Entry, Group, Time} from './timetable';
+import type {EntryClickedEvent} from './timetable';
+
+export {Entity, Entry, Group, Time, Timetable};
+export type {EntryClickedEvent};
 export default Timetable;
